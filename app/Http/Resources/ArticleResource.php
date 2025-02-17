2bf5426e1b030fd9aeba033f806a1bab4733e175
fernando <vluisfernando62@gmail.com>
2025-02-17 19:48:39 -0400

    feat: implement fetching a single article

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'articles',
+            'id' => (string) $this->resource->getRouteKey(),
+            'attributes' => [
+                'title' => $this->resource->title,
+                'slug' => $this->resource->slug,
+                'content' => $this->resource->content,
+            ],
+            'links' => [
+                'self' => route('api.v1.articles.show', $this->resource),
+            ],
+        ];
+    }
+}
